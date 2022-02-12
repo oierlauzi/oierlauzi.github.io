973ce5ebc5cd61342ba3ce455eb9e600b979cfea
Oier Lauzirika Zarrabeitia <oier.lauzirika.zarrabeitia@alumnos.upm.es>
2022-02-12 13:21:03 +0100

    Added a navbar

---
+++ src/components/TheNavbar.vue
@@ -0,0 +1,27 @@
+<template>
+	<div>
+		<b-navbar toggleable="lg" type="dark" variant="primary">
+			<b-navbar-brand>Oier Lauzirika Zarrabeitia</b-navbar-brand>
+
+			<!-- Next options are collapsed for small screens -->
+			<b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+			<b-collapse id="nav-collapse" is-nav>
+				<!-- Left aligned nav items -->
+
+				<!-- Right aligned nav items -->
+				<b-navbar-nav class="ml-auto">
+					<b-nav-item-dropdown text="Lang" right>
+						<b-dropdown-item>Example lang</b-dropdown-item>
+					</b-nav-item-dropdown>
+				</b-navbar-nav>
+			</b-collapse>
+		</b-navbar>
+	</div>
+</template>
+
+<script lang="ts">
+	import { Component, Vue } from 'vue-property-decorator';
+
+	@Component
+	export default class TheNavbar extends Vue {}
+</script>
--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,17 @@
 import '@babel/polyfill'
 import 'mutationobserver-shim'
 import Vue from 'vue'
-import './plugins/bootstrap-vue'
-import App from './App.vue'
+
 import router from './router'
 import store from './store'
+import './plugins/bootstrap-vue'
+
+import App from './App.vue'
 
 Vue.config.productionTip = false
 
 new Vue({
-  router,
-  store,
-  render: h => h(App)
+	router,
+	store,
+	render: h => h(App)
 }).$mount('#app')
